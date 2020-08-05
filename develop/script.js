2dc31ad5da198b3484c55fca441e36ef555c04a8
Haynes Brotherton <hbrotherton44@gmail.com>
2020-08-04 21:25:43 -0400

    added functionality to getforecast and searchweather

--- develop/script.js
+++ develop/script.js
@@ -24,8 +24,8 @@ $(document).ready(function() {
 
   function searchWeather(searchValue) {
     $.ajax({
-      type: "",
-      url: "" + searchValue + "",
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=0df21c98a0faf830fca56b4a59a1c375",
       dataType: "json",
       success: function(data) {
         // create history link for this search
@@ -37,11 +37,27 @@ $(document).ready(function() {
         }
         
         // clear any old content
+        $('#today').empty();
+
+        var unixTime = data.dt;
+        var milliseconds = unixTime * 1000;
+        var dateConv = new Date(milliseconds);
+        var fixedDate = dateConv.toLocaleDateString();
 
         // create html content for current weather
+        var cityName = $("<h3>").text(data.name + " (" + fixedDate + ")");
+        var iconNum = data.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
+        var weatherIcon = $("<img>").attr("src", iconURL);        
+        cityName.append(weatherIcon);
+
+        var temp = $("<p>").html("Temperature: " + data.main.temp + "&#8457;");
+        var humidity = $("<p>").text("Humidity: " + data.main.humidity + "%");
+        var wind = $("<p>").text("Wind Speed: " + data.wind.speed + "MPH");
 
         // merge and add to page
-        
+        $("#today").append(cityName, temp, humidity, wind);
+
         // call follow-up api endpoints
         getForecast(searchValue);
         getUVIndex(data.coord.lat, data.coord.lon);
@@ -51,20 +67,42 @@ $(document).ready(function() {
   
   function getForecast(searchValue) {
     $.ajax({
-      type: "",
-      url: "" + searchValue + "",
+      type: "GET",
+      url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=0df21c98a0faf830fca56b4a59a1c375",
       dataType: "json",
       success: function(data) {
         // overwrite any existing content with title and empty row
+        $('#forecast').empty();
+        var title = $("<h3>").text("5 Day Forecast:");
+        var newRow = $("<div>").attr("class", "row").attr("id", "card-row");
+        $("#forecast").append(title, newRow);
 
         // loop over all forecasts (by 3-hour increments)
         for (var i = 0; i < data.list.length; i++) {
           // only look at forecasts around 3:00pm
           if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-            // create html elements for a bootstrap card
             
+            var currentDay = data.list[i];
+
+            // Convert time data to date
+            var unixTime = currentDay.dt;
+            var milliseconds = unixTime * 1000;
+            var dateConv = new Date(milliseconds);
+            var fixedDate = dateConv.toLocaleDateString();
+
+
+            // create html elements for a bootstrap card
+            var card = $("<div>").attr("class", "card-body");
+            var date = $("<h4>").text(fixedDate);
+            var iconNum = currentDay.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/wn/" + iconNum + "@2x.png";
+            var iconCard = $("<img>").attr("src", iconURL); 
+            var humidCard = $("<p>").text("Humidity: " + currentDay.main.humidity + "%");
+            var tempCard = $("<p>").html("Temp: " + currentDay.main.temp + "&#8457;");
 
             // merge together and put on page
+            card.append(date, iconCard, tempCard, humidCard);
+            $("#card-row").append(card);
           }
         }
       }
@@ -73,8 +111,8 @@ $(document).ready(function() {
 
   function getUVIndex(lat, lon) {
     $.ajax({
-      type: "",
-      url: "" + lat + "&lon=" + lon,
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,daily&appid=0df21c98a0faf830fca56b4a59a1c375",
       dataType: "json",
       success: function(data) {
         var uv = $("<p>").text("UV Index: ");
@@ -82,6 +120,7 @@ $(document).ready(function() {
         
         // change color depending on uv value
         
+
         $("#today .card-body").append(uv.append(btn));
       }
     });
